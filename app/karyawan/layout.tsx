82b15ec6b98ddfa99811a222cee6243743f68bcc
alexdoffgit <noreply@plea.se>
2024-04-08 20:44:58 +0700

    create layout for karyawan route

---
+++ app/karyawan/layout.tsx
@@ -0,0 +1,14 @@
+import SidenavCenterLayout from "@/components/layouts/sidenav-center";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode
+}
+
+export default function KaryawanLayout({ children }: Props) {
+  return (
+    <SidenavCenterLayout tipe="karyawan">
+      {children}
+    </SidenavCenterLayout>
+  )
+}
