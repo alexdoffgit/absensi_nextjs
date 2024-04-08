eac0510ed2529669911c1df68184c74a68b5afbc
alexdoffgit <noreply@plea.se>
2024-04-08 20:44:31 +0700

    add dummy data for view

---
+++ dummy-data/absen-karyawan.ts
@@ -0,0 +1,22 @@
+import { promises as fs } from "fs";
+import { z } from "zod";
+
+const AbsenKaryawanSchema = z.array(
+    z.object({
+        tanggal: z.string(),
+        come_time: z.string(),
+        leave_time: z.string(),
+        jam_masuk: z.string(),
+        jam_keluar: z.string(),
+        status: z.string()
+    })
+)
+
+type AbsenKaryawan = z.infer<typeof AbsenKaryawanSchema>
+
+export async function allAbsenKaryawan(): Promise<AbsenKaryawan> {
+    const file = await fs.readFile(process.cwd() + '/dummy-data/absen-karyawan.json', 'utf8')
+    const absenKaryawan = JSON.parse(file)
+    AbsenKaryawanSchema.parse(absenKaryawan.data)
+    return absenKaryawan.data
+}
