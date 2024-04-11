a1f95cbc0e42d89bb4f769d7a3a567752bb7cc39
alexdoffgit <noreply@plea.se>
2024-04-11 18:13:53 +0700

    add tabel absensi page

--- app/karyawan/absensi/page.tsx
+++ app/karyawan/absensi/page.tsx
@@ -1,4 +1,16 @@
 import { allAbsenKaryawan } from "@/dummy-data/absen-karyawan";
+import Filter from "@/components/icons/filter";
+import Download from "@/components/icons/download";
+
+export default async function AbsensiKaryawan() {
+  return (
+    <div className="grid grid-cols-7 lg:grid-cols-10 h-full">
+      <div className="col-start-2 col-end-7 lg:col-end-10">
+        <DataTable />
+      </div>
+    </div>
+  );
+}
 
 async function DataTable() {
   const data = await allAbsenKaryawan();
@@ -7,69 +19,72 @@ async function DataTable() {
   const tdStyle = `text-center p-2`;
   const tdOddStyle = `${tdStyle} bg-slate-200`;
 
-  const FilterControl = () => (
-    <div className="flex gap-2 my-2">
-      <button className="p-2 bg-blue-600 text-white rounded">Minggu ini</button>
-      <button className="p-2 bg-blue-600 text-white rounded">Bulan ini</button>
-    </div>
-  );
-
   return (
-    <div className="flex flex-col">
-      <FilterControl />
-      <div className="overflow-x-auto">
-        <table className="w-full">
-          <thead>
-            <tr>
-              <th className={thStyle}>Tanggal</th>
-              <th className={thStyle}>Come Time</th>
-              <th className={thStyle}>Leave Time</th>
-              <th className={thStyle}>Jam Masuk</th>
-              <th className={thStyle}>Jam Keluar</th>
-              <th className={thStyle}>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data
-              .map((d, i) => {
-                if (i % 2 != 0) {
-                  return (
-                    <tr key={`${d.tanggal}__${d.come_time}__${d.leave_time}`}>
-                      <td className={tdOddStyle}>{d.tanggal}</td>
-                      <td className={tdOddStyle}>{d.come_time}</td>
-                      <td className={tdOddStyle}>{d.leave_time}</td>
-                      <td className={tdOddStyle}>{d.jam_masuk}</td>
-                      <td className={tdOddStyle}>{d.jam_keluar}</td>
-                      <td className={tdOddStyle}>{d.status}</td>
-                    </tr>
-                  );
-                } else {
-                  return (
-                    <tr key={`${d.tanggal}__${d.come_time}__${d.leave_time}`}>
-                      <td className={tdStyle}>{d.tanggal}</td>
-                      <td className={tdStyle}>{d.come_time}</td>
-                      <td className={tdStyle}>{d.leave_time}</td>
-                      <td className={tdStyle}>{d.jam_masuk}</td>
-                      <td className={tdStyle}>{d.jam_keluar}</td>
-                      <td className={tdStyle}>{d.status}</td>
-                    </tr>
-                  );
-                }
-              })
-              .slice(0, 10)}
-          </tbody>
-        </table>
+    <div>
+      <h1 className="text-3xl mb-14 mt-6">Absen Karyawan</h1>
+      <div className="flex flex-col">
+        <FilterControl />
+        <div className="overflow-x-auto">
+          <table className="w-full">
+            <thead>
+              <tr>
+                <th className={thStyle}>Tanggal</th>
+                <th className={thStyle}>Come Time</th>
+                <th className={thStyle}>Leave Time</th>
+                <th className={thStyle}>Jam Masuk</th>
+                <th className={thStyle}>Jam Keluar</th>
+                <th className={thStyle}>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data
+                .map((d, i) => {
+                  if (i % 2 != 0) {
+                    return (
+                      <tr key={`${d.tanggal}__${d.come_time}__${d.leave_time}`}>
+                        <td className={tdOddStyle}>{d.tanggal}</td>
+                        <td className={tdOddStyle}>{d.come_time}</td>
+                        <td className={tdOddStyle}>{d.leave_time}</td>
+                        <td className={tdOddStyle}>{d.jam_masuk}</td>
+                        <td className={tdOddStyle}>{d.jam_keluar}</td>
+                        <td className={tdOddStyle}>{d.status}</td>
+                      </tr>
+                    );
+                  } else {
+                    return (
+                      <tr key={`${d.tanggal}__${d.come_time}__${d.leave_time}`}>
+                        <td className={tdStyle}>{d.tanggal}</td>
+                        <td className={tdStyle}>{d.come_time}</td>
+                        <td className={tdStyle}>{d.leave_time}</td>
+                        <td className={tdStyle}>{d.jam_masuk}</td>
+                        <td className={tdStyle}>{d.jam_keluar}</td>
+                        <td className={tdStyle}>{d.status}</td>
+                      </tr>
+                    );
+                  }
+                })
+                .slice(0, 10)}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
 }
 
-export default async function AbsensiKaryawan() {
+function FilterControl() {
   return (
-    <div className="grid grid-cols-7 lg:grid-cols-10 h-full">
-      <div className="col-start-2 col-end-7 lg:col-end-10">
-        <DataTable />
-      </div>
+    <div className="flex gap-2 my-2">
+      <button className="p-2 bg-blue-600 text-white rounded">Minggu ini</button>
+      <button className="p-2 bg-blue-600 text-white rounded">Bulan ini</button>
+      <button className="p-2 bg-blue-600 text-white rounded flex gap-1">
+        <Filter className="w-4" />
+        Filter
+      </button>
+      <button className="p-2 bg-blue-600 text-white rounded flex gap-1 items-center">
+        <Download />
+        Download Absen
+      </button>
     </div>
   );
 }
